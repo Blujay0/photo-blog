ff2895bf2c4fbe93113bb9c94fc461a7948cbf86
Jay Lim <jlimoman@gmail.com>
2023-07-31 16:26:54 -0600

    render initial landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
-import { Swtich, Route, Redirect } from 'react-router-dom'
+import LandingPage from './LandingPage';
+import { Routes, Route, Redirect } from 'react-router-dom';
 
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a> */}
-        
-      </header>
+        </a>
+      </header> */}
+      <Routes>
+        <Route exact path="/" element={<LandingPage />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <>
+      <nav>Jay/Lim</nav>
+      {/* Massimo Margagnoni */}
+      <img alt='lakeshore' src="http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg"/>
+      <img alt='waterfall' src="http://c2.staticflickr.com/8/7218/7209301894_c99d3a33c2_h.jpg"/>
+      <img alt='desert' src="http://c2.staticflickr.com/8/7231/6947093326_df216540ff_b.jpg"/>
+
+      {/* Giuseppe Milo */}
+      <img alt='airport' src="http://c1.staticflickr.com/9/8788/17367410309_78abb9e5b6_b.jpg"/>
+      <img alt='cyclist' src="http://c2.staticflickr.com/6/5814/20700286354_762c19bd3b_b.jpg"/>
+      <img alt='building next to river' src="http://c2.staticflickr.com/6/5647/21137202535_404bf25729_b.jpg"/>
+
+      {/* GÃķrlitzPhotography */}
+      <img alt='painter on ladder' src="http://c2.staticflickr.com/6/5588/14991687545_5c8e1a2e86_b.jpg"/>
+      <img alt='a lock' src="http://c2.staticflickr.com/4/3888/14878097108_5997041006_b.jpg"/>
+      <img alt='towed car' src="http://c2.staticflickr.com/8/7579/15482110477_0b0e9e5421_b.jpg"/>
+
+      {/* All Photos Licensed Under Creative Commons 2.0
+      https://creativecommons.org/licenses/by/2.0/legalcode */}
+    </>
+
+  )
+}
+
+export default LandingPage
