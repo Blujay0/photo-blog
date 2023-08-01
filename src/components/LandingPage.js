2f254c25161d1f70605587cd8e562f7d7bec648c
Jay Lim <jlimoman@gmail.com>
2023-07-31 22:10:07 -0600

    modify landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,7 +4,7 @@ import './LandingPage.css';
 const LandingPage = () => {
   return (
     <>
-      <nav>Jay/Lim</nav>
+      <nav>Photography/Blog</nav>
       {/* Niels Baars */}
       <img alt='chilling Meerkat' src="https://images.unsplash.com/photo-1689793956921-3c59783b4e6a"/>
       <img alt='prairie dog eating' src="https://images.unsplash.com/photo-1689790233876-ad83cddc83db"/>
@@ -20,10 +20,19 @@ const LandingPage = () => {
       <img alt='green gecko' src="https://images.unsplash.com/photo-1688417158297-ff0fe1a0dfee"/>
       <img alt='snowleopard looking up' src="https://images.unsplash.com/photo-1688417143495-6b0891fb58fa"/>
 
+      {/* Sir. Simo */}
+      <img alt='jellyfish' src="https://images.unsplash.com/photo-1665928768588-7919f0403bc8"/>
+      <img alt='a trio of Japanese carp' src="https://images.unsplash.com/photo-1673098779245-6e9fd9f7e80b"/>
+      <img alt='a highland cow lying down' src="https://images.unsplash.com/photo-1686572602329-93a31a1e2e4f"/>
+
+      {/* Siegfried Poepperl */}
+      <img alt='great blue heron' src="https://images.unsplash.com/photo-1688656690484-e76f22507f96"/>
+      <img alt='peacock' src="https://images.unsplash.com/photo-1686642296451-b85674199958"/>
+      <img alt='blue jay' src="https://images.unsplash.com/photo-1623156584604-ec56b8f92fcb"/>
+
       {/* All Photos Licensed Under Creative Commons 2.0
       https://creativecommons.org/licenses/by/2.0/legalcode */}
     </>
-
   )
 }
 
